9f223751a23bbc02a433642564d361babd8d35f8
muthu1712 <muthu1712@yahoo.com>
2020-07-27 16:38:44 +0530

    Added admin routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,25 +1,63 @@
 const router = require('express').Router();
-const { personService, userService } = require('../services');
+const { userService } = require('../services');
 
-router.get('/persons/:id', async(req, res) => {
-    const { id } = req.params;
+router.get('/users/:login', async(req, res) => {
+    const { login } = req.params;
+    console.log(login);
     try {
-    const person = await personService.getById(id);
-    res.send(person).status(200);
+        const user = await userService.getByLogin(login);
+        return res.send(user).status(200);
     } catch(e) {
-        res.json({
+        return res.json({
             message: e.message
         }).status(500);
     }
 });
 
-router.get('/users/:id', async(req, res) => {
+router.post('/users', async(req, res) => {
+    const payload = req.body;
+    try {
+        const [record] = await userService.upsert(payload);
+        return res.send(record).status(200);
+    } catch(e) {
+        return res.json({
+            message: e.message
+        }).status(500);
+    }
+});
+
+router.delete('/users/:id', async(req, res) => {
     const { id } = req.params;
     try {
-    const user = await userService.getById(id);
-    res.send(user).status(200);
+        await userService.deleteUserById(id);
+        return res.json({}).status(200);
     } catch(e) {
-        res.json({
+        return res.json({
+            message: e.message
+        }).status(500);
+    }
+});
+
+router.get('/users/:id/persons', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const persons = await userService.getPersonsByUser(id);
+        const personslist = persons.map(p => p._person);
+        return res.send(personslist).status(200);
+    } catch(e) {
+        return res.json({
+            message: e.message
+        }).status(500);
+    }
+});
+
+router.post('/users/persons/transfer', async(req, res) => {
+    try {
+        const payload = req.body;
+        const persons = await userService.transferPersonsToUser(payload);
+        return res.send(persons).status(200);
+    } catch(e) {
+        return res.json({
             message: e.message
         }).status(500);
     }
